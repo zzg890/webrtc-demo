5aedad1c9415c33fadafdb2c4cb8bdcc5ebfe79a
zzg890 <zzg890@outlook.com>
2022-10-18 23:05:35 +0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare class WebRtcStreamer {
+  constructor(id: string, url: string)
+  connect(url: string): void
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    var streamer = new WebRtcStreamer("video", location.protocol + "//" + window.location.hostname + ":8000");
+    streamer.connect("");
+  }
   title = 'webrtc-demo';
 }
